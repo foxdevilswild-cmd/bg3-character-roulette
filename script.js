eceb8668cfdbdf71db6a11d12cd65e769e5b3654
foxdevilswild-cmd <jennyschumacher1990@googlemail.com>
2026-07-17 13:10:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -82,17 +82,95 @@ function getRandomClass(){
 
 
 function getRandomBackground(){
+    function getRaceFeatures(raceName){
+
+    return DATA.features[raceName];
+
+}
+    function randomAppearance(race){
+
+    const appearance = {};
+
+    const features = getRaceFeatures(race.name);
+
+    appearance.gender = random(DATA.appearance.appearance.genders);
+
+    appearance.bodyType = random(race.bodyTypes);
+
+    appearance.skin = random(DATA.appearance.appearance.skinTones);
+
+    appearance.eye = random(DATA.appearance.appearance.eyeColors);
+
+    appearance.hairColor = random(DATA.appearance.appearance.hairColors);
+
+    appearance.voice = random(DATA.voices.voices);
+
+    appearance.face = "Zufällig";
+
+    appearance.hair = "Zufällig";
+
+    appearance.tattoo = random(DATA.appearance.appearance.tattoos);
+
+    appearance.scar = random(DATA.appearance.appearance.scars);
+
+    appearance.makeup = random(DATA.appearance.appearance.makeup);
+
+    appearance.piercing = random(DATA.appearance.appearance.piercings);
+
+
+
+    if(features.beard){
+
+        appearance.beard="Zufällig";
+
+    }else{
+
+        appearance.beard="-";
+
+    }
+
+
+
+    if(features.horns){
+
+        appearance.horns=random(DATA.horns.hornStyles);
+
+    }else{
+
+        appearance.horns="-";
+
+    }
+
+
+
+    if(features.tail){
+
+        appearance.tail=random(DATA.tails.dragonbornTails);
+
+    }else{
+
+        appearance.tail="-";
+
+    }
+
+
+
+    return appearance;
+
+}
 
     return random(DATA.backgrounds.backgrounds);
 
 }
 function generatePlayer(number){
 
-    const race=getRandomRace();
+    const race = getRandomRace();
 
-    const playerClass=getRandomClass();
+    const playerClass = getRandomClass();
 
-    const background=getRandomBackground();
+    const background = getRandomBackground();
+
+    const appearance = randomAppearance(race);
 
 
 
@@ -112,7 +190,28 @@ function generatePlayer(number){
 
 
 
+    set("gender"+number,appearance.gender);
+
+    set("bodytype"+number,appearance.bodyType);
+
+    set("voice"+number,appearance.voice);
+
+    set("face"+number,appearance.face);
+
+    set("hair"+number,appearance.hair);
+
+    set("hairColor"+number,appearance.hairColor);
+
+    set("eyeColor"+number,appearance.eye);
+
+    set("skinColor"+number,appearance.skin);
+
+    set("tattoo"+number,appearance.tattoo);
+
+    set("scar"+number,appearance.scar);
+
 }
+
 function generateGuardian(){
 
     const race=getRandomRace();
